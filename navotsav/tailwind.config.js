f343cfd64647841fc871d1295d552545110fb8a2
bharatravihal <ravihalbharat@gmail.com>
2024-09-21 15:07:25 +0530

    m

--- navotsav/tailwind.config.js
+++ navotsav/tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}", // Yeh path sahi hona chahiye
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightblue: {
+          200: '#A6D8E7', // Example light blue color
+        },
+        lightorange: {
+          200: '#FDD7B9', // Example light orange color
+        },
+      },
+    },
   },
   plugins: [],
 };
