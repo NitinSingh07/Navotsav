cf2a72b7d2d54b6ccf28ead7a8f31e2d24194be5
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2024-09-21 23:34:16 +0530

    Refactor tagline component to update image source and add registration link

--- navotsav/src/components/tagline.jsx
+++ navotsav/src/components/tagline.jsx
@@ -15,9 +15,9 @@ export const Tagline = () => {
       {/* Content Overlay */}
       <div className="relative text-center px-6 space-y-3 z-10">
         {/* Navotsav Heading */}
-        <div className="mb-96">
+        <div className="mb-80">
           <h1
-            className="text-8xl font-extrabold  text-white"
+            className="text-8xl font-extrabold mb-8 text-white"
             style={{
               textShadow:
                 "0 0 10px rgba(0, 112, 255, 0.8), 0 0 20px rgba(0, 112, 255, 0.8)", // Blue glow effect
@@ -25,7 +25,7 @@ export const Tagline = () => {
           >
             Navotsav
           </h1>
-          <h1 className="text-4xl md:text-5xl font-serif  text-black">2024</h1>
+          <h1 className="text-4xl md:text-5xl font-serif text-black">2024</h1>
         </div>
 
         {/* Main Tagline */}
@@ -36,9 +36,15 @@ export const Tagline = () => {
         </h1>
 
         {/* CTA Button */}
-        <button className="bg-blue-600 hover:bg-blue-700 text-white py-3 px-8 rounded-full shadow-lg transition-all duration-300 transform hover:scale-105">
-          Register Now
-        </button>
+        <a
+          href="https://id-card-generator-silk.vercel.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="bg-blue-600 hover:bg-blue-700 text-white py-3 px-8 rounded-full shadow-lg transition-all duration-300 transform hover:scale-105">
+            Register Now
+          </button>
+        </a>
 
         {/* Decorative Line */}
         <div className="h-1 w-40 mx-auto bg-blue-600 rounded-lg mt-8"></div>
