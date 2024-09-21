9204767c0b2928d5735aa2f431a0b528e3f55d2e
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2024-09-21 23:25:31 +0530

    Refactor main.jsx to update component imports and remove commented code

--- navotsav/src/main.jsx
+++ navotsav/src/main.jsx
@@ -3,19 +3,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Tagline } from "./components/tagline.jsx";
-import { EventDetails } from "./components/Event.jsx";
 import { EventAgenda } from "./components/agenda.jsx";
-// import {FeedbackForm} from "./components/feebackForm.jsx"
-// import {FeedbackForm} from "./components/feedBackForm.jsx"
+
 import FeedbackForm from "./components/feedBackForm.jsx";
 import TeamMembers from "./components/team.jsx";
+import { EventDetails } from "./components/Event.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Tagline />
     <EventDetails />
     <EventAgenda />
-    {/* <FeedbackForm /> */}
     <TeamMembers />
     <FeedbackForm />
   </StrictMode>
