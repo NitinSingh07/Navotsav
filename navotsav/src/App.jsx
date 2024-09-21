313fd7c6491e5b15aa9edb6b835f7bfebcfe0f97
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2024-09-21 13:55:01 +0530

    Refactor routing in App component

--- navotsav/src/App.jsx
+++ navotsav/src/App.jsx
@@ -3,14 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Tagline } from "./components/tagline";
 import { EventDetails } from "./components/Event";
 
-
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Tagline />} />
-        <Route path="/event" element={<EventDetails/>} />
-        
+        <Route path="/event" element={<EventDetails />} />
       </Routes>
     </Router>
   );
