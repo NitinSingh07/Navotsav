9cbf3c8d5e57e682186a37d3d193b0061c3e96f3
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2024-09-21 23:44:41 +0530

    Refactor tagline component to update image source and adjust spacing

--- navotsav/src/components/tagline.jsx
+++ navotsav/src/components/tagline.jsx
@@ -13,11 +13,11 @@ export const Tagline = () => {
       </div>
 
       {/* Content Overlay */}
-      <div className="relative text-center px-6 space-y-3 z-10">
+      <div className="relative text-center px-6 space-y-6 z-10">
         {/* Navotsav Heading */}
-        <div className="mb-80">
+        <div className="mb-64 md:mb-80">
           <h1
-            className="text-8xl font-extrabold mb-8 text-white"
+            className="text-8xl font-extrabold mb-4 text-white"
             style={{
               textShadow:
                 "0 0 10px rgba(0, 112, 255, 0.8), 0 0 20px rgba(0, 112, 255, 0.8)", // Blue glow effect
@@ -25,11 +25,13 @@ export const Tagline = () => {
           >
             Navotsav
           </h1>
-          <h1 className="text-4xl md:text-5xl font-serif text-black">2024</h1>
+          <h1 className="text-4xl md:text-5xl font-serif mb-8 text-black">
+            2024
+          </h1>
         </div>
 
         {/* Main Tagline */}
-        <h1 className="text-3xl md:text-6xl font-extrabold ">
+        <h1 className="text-3xl md:text-6xl font-extrabold mb-24">
           <span className="text-orange-500">Connect.</span>
           <span className="text-blue-500"> Collaborate.</span>
           <span className="text-green-500"> Contribute.</span>
@@ -41,13 +43,13 @@ export const Tagline = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <button className="bg-blue-600 hover:bg-blue-700 text-white py-3 px-8 rounded-full shadow-lg transition-all duration-300 transform hover:scale-105">
+          <button className="bg-blue-600 hover:bg-blue-700 text-white py-3 px-8 rounded-full shadow-lg transition-all duration-300 transform hover:scale-105 mt-8">
             Register Now
           </button>
         </a>
 
         {/* Decorative Line */}
-        <div className="h-1 w-40 mx-auto bg-blue-600 rounded-lg mt-8"></div>
+        <div className="h-1 w-40 mx-auto bg-blue-600 rounded-lg mt-4"></div>
       </div>
     </section>
   );
